87c5aa5198a2cd79ea22fb790e1b6775b1cf7e81
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-02 21:08:22 +0500

    chapter 1 to 9 complete...

---
+++ JavaScript/class 02/app.js
@@ -0,0 +1,148 @@
+
+// alert() js builtin function
+
+// alert(faraz);
+
+// alert("hellomyfriend")
+
+// document.write("helloworld");
+
+// console.log("hello world");
+
+// runtime error
+
+// compile time error
+
+
+
+
+// document.write(); js builtin method
+
+// console.log(); js builtin method
+
+
+// builtin
+
+// user defined
+
+
+// variables
+
+// data types
+
+// string ""
+// number 50;
+
+// var names = 'faraz';
+
+// console.log(names);
+
+
+// variable declaration
+
+// var number;
+
+// console.log(number);
+
+
+// variable declaration and initialization
+
+// var number1 = 50;
+
+// console.log(number1);
+
+
+// variable legal ways
+
+
+// camel case ===> fullName, lastName, firstName
+
+// snake case ===> full_name,last_name,first_name
+
+// pascal case ===> FullName, LastName, FirstName
+
+
+
+
+
+// var firstName = "Muhammad";
+
+// var lastName = "Faraz";
+
+// console.log(firstName , lastName);
+
+// js  +  ==> add,concat
+
+// var num1 = "10";
+
+// var num2 = "2";
+
+// console.log(num1 + num2);
+
+
+
+// variable illegal ways
+
+// var first name = "faraz";
+
+
+// var num1 = 10 , num2 = 20;
+
+// console.log(num1 - num2);
+
+
+
+
+
+// increment ++  +1
+
+//  decrement -- -1;
+
+
+// var number = 10;
+
+// number++;
+
+// console.log(number);
+
+
+
+// number =  number + 1;
+
+// var update = number++
+
+// console.log(update);
+
+
+// var num = 2;
+
+// var exp = num - (--num + ++num);
+            
+// console.log(exp);
+
+
+
+
+// prompt() js builtin function
+
+
+var num1 = +prompt();
+
+var num2 = +prompt();
+
+console.log(num1 + num2);
+
+
+
+// dynamic programming
+
+// static programming
+
+
+var firstName = prompt("Enter your first name");
+
+var lastName = prompt("Enter your last name");
+
+console.log(firstName + " " + lastName);
+
+
