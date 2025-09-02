8b5cf1f045d0f927485cd535ea4b82dfc6140389
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-03 01:37:08 +0500

    Update app.js

--- JavaScript/class 02/app.js
+++ JavaScript/class 02/app.js
@@ -1,5 +1,5 @@
 // alert() js builtin function
-
+ 
 alert(faraz);  // error occur 
 
 alert("hellomyfriend")
@@ -145,4 +145,5 @@ var firstName = prompt("Enter your first name");
 var lastName = prompt("Enter your last name");
 
 console.log(firstName + " " + lastName);
+
 
