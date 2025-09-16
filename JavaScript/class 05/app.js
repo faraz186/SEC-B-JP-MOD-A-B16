f9a42f1f3ea70ab4561407cd887ccfefd9b586ce
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-09-17 00:24:05 +0500

    Update app.js

--- JavaScript/class 05/app.js
+++ JavaScript/class 05/app.js
@@ -1,5 +1,5 @@
 var userInput = prompt("Enter a month");
-          
+           
 var inputLen = userInput.length;
 
 var shortform;
@@ -119,6 +119,7 @@ console.log(typeof num.toString());
 var random = Math.random();
 
 console.log(parseFloat(random.toFixed(5)));
+
 
 
 
