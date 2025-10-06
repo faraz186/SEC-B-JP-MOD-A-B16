8bf70964e7cdb0cf1428c1392c91822f44b900e4
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-06 10:47:52 +0500

    Update app.js

--- JavaScript/class 09/app.js
+++ JavaScript/class 09/app.js
@@ -1,5 +1,5 @@
 function submit() {
-  var input = document.getElementById("inp");
+  var input = document.getElementById("inp"); 
 
   console.log(input.value);
 }
@@ -103,3 +103,4 @@ function greet() {
 }
 
 setTimeout(greet, 3000);
+
