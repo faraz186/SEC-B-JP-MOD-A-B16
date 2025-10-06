86035d93a35c20273e5f6d336b29dc0dbb75f5a6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-06 10:47:37 +0500

    Update app.js

--- JavaScript/class 09/app.js
+++ JavaScript/class 09/app.js
@@ -1,82 +1,82 @@
-// function submit() {
-//   var input = document.getElementById("inp");
+function submit() {
+  var input = document.getElementById("inp");
 
-//   console.log(input.value);
-// }
+  console.log(input.value);
+}
 
-// var h2Element = document.getElementById("heading");
-// var pElement = document.getElementById("para");
-// var aElement = document.getElementById("link");
+var h2Element = document.getElementById("heading");
+var pElement = document.getElementById("para");
+var aElement = document.getElementById("link");
 
-// console.log(h2Element.innerHTML);
-// console.log(pElement.innerHTML);
-// console.log(aElement.innerHTML);
+console.log(h2Element.innerHTML);
+console.log(pElement.innerHTML);
+console.log(aElement.innerHTML);
 
 // diff between innerHTML / innerText
 
-// var divElement = document.getElementById("main");
+var divElement = document.getElementById("main");
 
-// console.log(divElement);
+console.log(divElement);
 
-// divElement.innerText = "<h1>Hello WOrld</h1>";
+divElement.innerText = "<h1>Hello WOrld</h1>";
 
-// function completePara() {
-//   var pElement = document.getElementById("para");
-//   var aElement = document.getElementById("link");
+function completePara() {
+  var pElement = document.getElementById("para");
+  var aElement = document.getElementById("link");
 
-//   if (aElement.innerHTML === "Read more") {
-//     pElement.innerHTML =
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus deserunt repellendus vel possimus cum aliquid tempore deleniti cumque molestias delectus.";
-//     aElement.innerHTML = "Read less";
-//   } else {
-//     pElement.innerHTML =
-//       "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
-//     aElement.innerHTML = "Read more";
-//   }
-// }
+  if (aElement.innerHTML === "Read more") {
+    pElement.innerHTML =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus deserunt repellendus vel possimus cum aliquid tempore deleniti cumque molestias delectus.";
+    aElement.innerHTML = "Read less";
+  } else {
+    pElement.innerHTML =
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+    aElement.innerHTML = "Read more";
+  }
+}
 
-// function makeInvisible(element) {
-//   element.className = "img";
-//   console.log(element);
-// }
+function makeInvisible(element) {
+  element.className = "img";
+  console.log(element);
+}
 
-// var link = document.getElementById("link");
+var link = document.getElementById("link");
 
-// link.innerText = "Google";
-// link.href = "https://www.google.com";
-// link.target = "_blank";
+link.innerText = "Google";
+link.href = "https://www.google.com";
+link.target = "_blank";
 
-// console.log(link);
+console.log(link);
 
-// var para = document.getElementById("para");
+var para = document.getElementById("para");
 
-// para.style.color = "red";
-// para.style.backgroundColor = "black";
-// para.style.borderRadius = "5px";
-// para.style.padding = "6px";
-// para.style.boxShadow = "0 0 10px red";
+para.style.color = "red";
+para.style.backgroundColor = "black";
+para.style.borderRadius = "5px";
+para.style.padding = "6px";
+para.style.boxShadow = "0 0 10px red";
 
-// var num = 2
+var num = 2
 
-// document.getElementById()
+document.getElementById()
 
-// var allParas = document.getElementsByTagName("p");
+var allParas = document.getElementsByTagName("p");
 
-// for (var i = 0; i < allParas.length; i++) {
-//   console.log(allParas[i]);
+for (var i = 0; i < allParas.length; i++) {
+  console.log(allParas[i]);
 
-//   allParas[i].style.color = "blue";
-// }
+  allParas[i].style.color = "blue";
+}
 
-// var allParas = document.getElementsByClassName("element");
+var allParas = document.getElementsByClassName("element");
 
-// console.log(allParas);
+console.log(allParas);
 
-// for (var i = 0; i < allParas.length; i++) {
-//   console.log(allParas[i]);
+for (var i = 0; i < allParas.length; i++) {
+  console.log(allParas[i]);
 
-//   allParas[i].style.color = "blue";
-// }
+  allParas[i].style.color = "blue";
+}
 
 // setInterval()
 
@@ -92,8 +92,6 @@ interval = setInterval(timer, 1000);
 function stop() {
   clearInterval(interval);
 }
-
-
 
 
 // setTimeout()
