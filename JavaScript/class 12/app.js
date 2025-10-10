e5607711a080e38c65e943422116b9b7b849fbf2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-11 01:30:31 +0500

    Update app.js

--- JavaScript/class 12/app.js
+++ JavaScript/class 12/app.js
@@ -1,140 +1,141 @@
-// var link = document.getElementById("anchor");
+var link = document.getElementById("anchor");
 
-// console.log(link.attributes[1]);
+console.log(link.attributes[1]);
 
 // array
 
 var arr = [10, "faraz", undefined, false, 500];
 
-// console.log(arr[3]);
+console.log(arr[3]);
 
 // objects
 
 // key:value pair
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-// };
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+};
 
-// console.log(obj.age);
+console.log(obj.age);
 
-// console.log(obj["email"]);
+console.log(obj["email"]);
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-// };
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+};
 
-// obj.courses = ["html", "css", "js"];
+obj.courses = ["html", "css", "js"];
 
-// obj.courses[3] = "php";
+obj.courses[3] = "php";
 
-// console.log(obj);
+console.log(obj);
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-//   address: {
-//     city: ["karachi", "lahore"],
-//     zipCode: 1234,
-//     logitude: 12,
-//     latitude: 14,
-//   },
-// };
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+  address: {
+    city: ["karachi", "lahore"],
+    zipCode: 1234,
+    logitude: 12,
+    latitude: 14,
+  },
+};
 
-// console.log(obj.address.city[1]);
+console.log(obj.address.city[1]);
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-// };
 
-// delete obj.email;
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+};
 
-// console.log(obj);
+delete obj.email;
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-// };
+console.log(obj);
 
-// var response = "city" in obj;
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+};
 
-// console.log(response);
+var response = "city" in obj;
 
-// var obj = {
-//   name: "faraz",
-//   email: "faraz@gmail.com",
-//   age: 25,
-//   getName: function () {
-//     console.log(obj.name);
-//   },
-// };
+console.log(response);
 
-// obj.getName();
+var obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  age: 25,
+  getName: function () {
+    console.log(obj.name);
+  },
+};
+
+obj.getName();
 
 // types of functions
 
 // 1) function declaration
 
-// function abc() {
-//   console.log("hello");
-// }
+function abc() {
+  console.log("hello");
+}
 
-// abc();
+abc();
 
 // 2) Annoymous function
 
-// var func = function () {
-//   console.log("hello");
-// };
+var func = function () {
+  console.log("hello");
+};
 
-// func();
+func();
 
 // 3) Arrow function
 
-// var stdObj1 = {
-//   stdName: "hamza",
-//   stdAge: 22,
-//   stdEmail: "hamza@gmail.com",
-//   stdGender: "male",
-// };
+var stdObj1 = {
+  stdName: "hamza",
+  stdAge: 22,
+  stdEmail: "hamza@gmail.com",
+  stdGender: "male",
+};
 
-// var stdObj2 = {
-//   stdName: "ali",
-//   stdAge: 22,
-//   stdEmail: "ali@gmail.com",
-//   stdGender: "male",
-// };
+var stdObj2 = {
+  stdName: "ali",
+  stdAge: 22,
+  stdEmail: "ali@gmail.com",
+  stdGender: "male",
+};
 
 // schema,model,blue print
 
 // constructor function
 
-// function Student(name, age, email, gender) {
-//   (this.stdName = name),
-//     (this.stdAge = age),
-//     (this.stdEmail = email),
-//     (this.stdGender = gender);
-// }
+function Student(name, age, email, gender) {
+  (this.stdName = name),
+    (this.stdAge = age),
+    (this.stdEmail = email),
+    (this.stdGender = gender);
+}
 
-// Student.prototype.getGender = function () {
-//   console.log("female");
-// };
+Student.prototype.getGender = function () {
+  console.log("female");
+};
 
-// var obj = new Student("faraz", 25, "faraz@gmail.com", "male");
+var obj = new Student("faraz", 25, "faraz@gmail.com", "male");
 
-// var obj2 = new Student("hamza", 20, "hamza@gmail.com", "male");
+var obj2 = new Student("hamza", 20, "hamza@gmail.com", "male");
 
-// console.log(obj);
+console.log(obj);
 
-// console.log(obj2);
+console.log(obj2);
 
 // bookName
 // bookAuthor
