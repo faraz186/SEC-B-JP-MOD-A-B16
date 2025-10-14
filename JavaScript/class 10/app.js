281184565a04ea00220837d98a256eca3328843b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-14 18:28:16 +0500

    stop watch app...

---
+++ JavaScript/class 10/app.js
@@ -0,0 +1,30 @@
+// var interval;
+
+// function greet() {
+//   console.log("hello students");
+// }
+
+// interval = setInterval(greet, 2000);
+
+// function stop() {
+//   clearInterval(interval);
+// }
+
+// setTimeout();
+
+// function message() {
+//   document.write("<i>welcome to our website</i>");
+// }
+
+// setTimeout(message, 5000);
+
+// document.getElementById();
+// document.getElementsByClassName();
+// document.getElementsByTagName();
+
+var anchorElement =
+  document.childNodes[0].childNodes[1].childNodes[0].childNodes[5];
+
+console.log(anchorElement);
+
+// text = junk artifact / garbage value
