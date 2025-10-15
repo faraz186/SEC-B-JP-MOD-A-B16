367260cac0ce4b4d535bf582fb81b30d6af4b44d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-15 08:17:04 +0500

    Update app.js

--- JavaScript/class 13/app.js
+++ JavaScript/class 13/app.js
@@ -1,74 +1,76 @@
 // objects;
 
-// var obj = {
-//   id: 1,
-//   name: "faraz",
-//   age: 25,
-// };
+var obj = {
+  id: 1,
+  name: "faraz",
+  age: 25,
+};
 
-// obj.city = "karachi";
+obj.city = "karachi";
 
-// delete obj.age;
+delete obj.age;
 
-// console.log("email" in obj);
+console.log("email" in obj);
 
-// var calc = {
-//   getSum: function () {
-//     return a + b;
-//   },
-//   getSub: function (a, b) {
-//     return a - b;
-//   },
-//   getMutli: function () {
-//     return a * b;
-//   },
-//   getDiv: function () {
-//     return a / b;
-//   },
-// };
 
-// console.log(calc.getSub(10, 50));
+var calc = {
+  getSum: function () {
+    return a + b;
+  },
+  getSub: function (a, b) {
+    return a - b;
+  },
+  getMutli: function () {
+    return a * b;
+  },
+  getDiv: function () {
+    return a / b;
+  },
+};
 
-// console.log(window);
+console.log(calc.getSub(10, 50));
 
-// function submit() {
-//   var input = document.getElementById("inp");
+console.log(window);
 
-//   if (input.value.trim() === "") {
-//     alert("missing field");
-//   } else {
-//     console.log(input.value);
-//   }
-// }
 
-// function greetWorld() {
-//   try {
-//     var greeting = "Hello world!";
-//     alert(greeting);
-//   } catch (error) {
-//     console.warn(error);
-//   }
-// }
-// greetWorld();
+function submit() {
+  var input = document.getElementById("inp");
 
-// var arr = [10,"abc"];
+  if (input.value.trim() === "") {
+    alert("missing field");
+  } else {
+    console.log(input.value);
+  }
+}
 
-// var arrayOfObject = [
-//   {
-//     id: 1,
-//     name: "faraz",
-//     city: "karachi",
-//   },
-//   {
-//     id: 2,
-//     name: "ali",
-//     city: "multan",
-//   },
-//   {
-//     id: 3,
-//     name: "hamza",
-//     city: "lahore",
-//   },
-// ];
+function greetWorld() {
+  try {
+    var greeting = "Hello world!";
+    alert(greeting);
+  } catch (error) {
+    console.warn(error);
+  }
+}
+greetWorld();
 
-// console.log(arrayOfObject);
+var arr = [10,"abc"];
+
+var arrayOfObject = [
+  {
+    id: 1,
+    name: "faraz",
+    city: "karachi",
+  },
+  {
+    id: 2,
+    name: "ali",
+    city: "multan",
+  },
+  {
+    id: 3,
+    name: "hamza",
+    city: "lahore",
+  },
+];
+
+console.log(arrayOfObject);
