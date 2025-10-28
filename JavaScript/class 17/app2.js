796b18c7f233166065ef86842a656b96934c1b99
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-10-28 22:19:48 +0500

    Update app2.js

--- JavaScript/class 17/app2.js
+++ JavaScript/class 17/app2.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+var firebaseConfig = {  
   apiKey: "AIzaSyBy6VhxVg31U833ivNWu1nOJQK6aBNnnyc",
   authDomain: "add-to-cart-46231.firebaseapp.com",
   databaseURL: "https://add-to-cart-46231-default-rtdb.firebaseio.com",
@@ -21,4 +21,5 @@ function signout() {
   localStorage.clear();
   window.location.href = "login.html";
 }
+
 
